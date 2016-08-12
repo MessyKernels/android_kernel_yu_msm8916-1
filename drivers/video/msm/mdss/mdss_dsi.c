CyanogenMod/android_kernel_yu_msm8916

msm: mdss: Add support for Yulong TPS65132 regulator
* Requires qcom,has-tps65132 entry to be set.

Change-Id: I7f883516c9b5e66b6cf3f78b9aadce9e0cad43f6

drivers/video/msm/mdss/mdss_dsi.c
@@ -60,6 +60,10 @@ static int mdss_dsi_regulator_init(struct platform_device *pdev)
 	return rc;
 }
 
+#if defined(CONFIG_MACH_YULONG) && defined(CONFIG_YL_TPS65132)
+extern void tps65132_config_set_to_tablet_mode(void);
+extern void tps65132_config_proc(void);
+#endif
 static int mdss_dsi_panel_power_on(struct mdss_panel_data *pdata, int enable)
 {
 	int ret = 0;
@@ -104,13 +108,20 @@ static int mdss_dsi_panel_power_on(struct mdss_panel_data *pdata, int enable)
 		if (!pdata->panel_info.mipi.lp11_init) {
 			if (mdss_dsi_pinctrl_set_state(ctrl_pdata, true))
 				pr_debug("reset enable: pinctrl not enabled\n");
-
+#if defined(CONFIG_MACH_YULONG) && defined(CONFIG_YL_TPS65132)
+		if (pdata->panel_info.mipi.has_tps65132)
+			tps65132_config_set_to_tablet_mode();
+#endif
 			ret = mdss_dsi_panel_reset(pdata, 1);
 			if (ret) {
 				pr_err("%s: Panel reset failed. rc=%d\n",
 						__func__, ret);
 				goto error_enable;
 			}
+#if defined(CONFIG_MACH_YULONG) && defined(CONFIG_YL_TPS65132)
+		if (pdata->panel_info.mipi.has_tps65132)
+			tps65132_config_proc();
+#endif
 		}
 	} else {
 		ret = mdss_dsi_panel_reset(pdata, 0);
@@ -759,7 +770,15 @@ int mdss_dsi_on(struct mdss_panel_data *pdata)
 	if (mipi->lp11_init) {
 		if (mdss_dsi_pinctrl_set_state(ctrl_pdata, true))
 			pr_debug("reset enable: pinctrl not enabled\n");
+#if defined(CONFIG_MACH_YULONG) && defined(CONFIG_YL_TPS65132)
+		if (mipi->has_tps65132)
+			tps65132_config_set_to_tablet_mode();
+#endif
 		mdss_dsi_panel_reset(pdata, 1);
+#if defined(CONFIG_MACH_YULONG) && defined(CONFIG_YL_TPS65132)
+		if (mipi->has_tps65132)
+			tps65132_config_proc();
+#endif
 	}
 
 	if (mipi->init_delay)

MessyKernels/android_kernel_yu_msm8916-1

drivers/video/msm/mdss/mdss_dsi.c
@@ -70,6 +70,10 @@ static int mdss_dsi_regulator_init(struct platform_device *pdev)
 	return rc;
 }
 
+#if defined(CONFIG_MACH_YULONG) && defined(CONFIG_REGULATOR_YL_TPS65132)
+extern void tps65132_config_set_to_tablet_mode(void);
+extern void tps65132_config_proc(void);
+#endif
 static int mdss_dsi_panel_power_off(struct mdss_panel_data *pdata)
 {
 	int ret = 0;
@@ -173,11 +177,18 @@ static int mdss_dsi_panel_power_on(struct mdss_panel_data *pdata)
 		!pdata->panel_info.mipi.lp11_init) {
 		if (mdss_dsi_pinctrl_set_state(ctrl_pdata, true))
 			pr_debug("reset enable: pinctrl not enabled\n");
-
+#if defined(CONFIG_MACH_YULONG) && defined(CONFIG_REGULATOR_YL_TPS65132)
+		if (pdata->panel_info.mipi.has_tps65132)
+			tps65132_config_set_to_tablet_mode();
+#endif
 		ret = mdss_dsi_panel_reset(pdata, 1);
 		if (ret)
 			pr_err("%s: Panel reset failed. rc=%d\n",
 					__func__, ret);
+#if defined(CONFIG_MACH_YULONG) && defined(CONFIG_REGULATOR_YL_TPS65132)
+		if (pdata->panel_info.mipi.has_tps65132)
+			tps65132_config_proc();
+#endif
 	}
 
 error:
@@ -618,7 +629,15 @@ int mdss_dsi_on(struct mdss_panel_data *pdata)
 	if (mipi->lp11_init) {
 		if (mdss_dsi_pinctrl_set_state(ctrl_pdata, true))
 			pr_debug("reset enable: pinctrl not enabled\n");
+#if defined(CONFIG_MACH_YULONG) && defined(CONFIG_REGULATOR_YL_TPS65132)
+		if (mipi->has_tps65132)
+			tps65132_config_set_to_tablet_mode();
+#endif
 		mdss_dsi_panel_reset(pdata, 1);
+#if defined(CONFIG_MACH_YULONG) && defined(CONFIG_REGULATOR_YL_TPS65132)
+		if (mipi->has_tps65132)
+			tps65132_config_proc();
+#endif
 	}
 
 	if (mipi->init_delay)